9384b6db35d03844bc85ebc64147950eb779b9cc
Jacqi Entzminger <jackie.entz@gmail.com>
2019-03-25 04:02:55 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,16 @@
+class Dog 
+ attr_accessor :name, :breed, :id
+ 
+ def initialize(name, breed, id = nil)
+    @id = id
+    @name = name
+    @breed = breed 
+  end 
+  
+  
+  
+  
+  
+  
+  
+end 
