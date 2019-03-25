2d36e91ad114b2da71cadc184e390099d1513b05
Jacqi Entzminger <jackie.entz@gmail.com>
2019-03-25 04:53:49 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,7 +3,6 @@ class Dog
   attr_accessor :id, :name, :breed
 
   def initialize(attributes)
-    #id: nil, name:, breed:
     attributes.each {|key, value| self.send(("#{key}="), value)}
     self.id ||= nil
   end
